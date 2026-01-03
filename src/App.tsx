43907bae4bbba7e613d8801613dee355fa7e765e
Trevor <executiveusa@gmail.com>
2026-01-03 08:30:32 -0600

    style: normalize import quotes to double quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Toaster } from '@/components/ui/toaster';
-import { Toaster as Sonner } from '@/components/ui/sonner';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
-import A2UIDemo from './pages/A2UIDemo';
-import NotFound from './pages/NotFound';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import A2UIDemo from "./pages/A2UIDemo";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
